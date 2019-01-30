9b3d97d4d0b9c37cd1ae99dd264e43d2a710c3df
Alan Pinhel <alanpinhel@gmail.com>
2019-01-30 14:18:17 -0200

    deploy mvp

---
+++ precache-manifest.e7025cc630596e6a0569446c4fbfbced.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d83a07ecc487a9662a2a",
+    "url": "/static/js/main.d83a07ec.chunk.js"
+  },
+  {
+    "revision": "c15fa55851052e5952f2",
+    "url": "/static/js/1.c15fa558.chunk.js"
+  },
+  {
+    "revision": "1d1abef63d7b927e7d3a76a3da2f37a8",
+    "url": "/index.html"
+  }
+];
