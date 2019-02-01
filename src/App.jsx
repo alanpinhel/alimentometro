d618872614edbc829cda03cc71738e4b192fbe24
Alan Pinhel <alanpinhel@gmail.com>
2019-02-01 15:50:15 -0200

    listagem dos alimentos

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,15 @@
 import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
 import aw from 'alimentometro-wrapper';
+import Foods from './components/Foods';
 
-class App extends React.Component {
-  state = {
-    foods: {},
-    addedFoods: [],
-  };
-
-  componentDidMount() {
-    this.setState({ foods: aw.getAlimentos() });
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    const { foods } = this.state;
-    const {
-      food: { value: food },
-      amount: { value: amount },
-    } = e.target;
-
-    const foodSelected = foods.find(f => f.id === +food);
-
-    this.setState(beforeState => {
-      return {
-        addedFoods: {
-          ...beforeState.addedFoods,
-          [food]: {
-            name: foodSelected.nome,
-            amount: aw.formata(foodSelected.pesoBruto * amount),
-          },
-        },
-      };
-    });
-  };
-
-  render() {
-    const { foods, addedFoods } = this.state;
-    return (
-      <React.Fragment>
-        <GlobalStyle />
-
-        <Header>
-          <Title>Alimentômetro</Title>
-          <Description>
-            Adicione o alimento e o peso em gramas da quantidade que você precisa consumir, para
-            saber quanto precisa comprar.
-          </Description>
-        </Header>
-
-        <Main>
-          <Form onSubmit={this.handleSubmit}>
-            <select id="food" name="food">
-              {Object.keys(foods).map(k => (
-                <option key={foods[k].id} value={foods[k].id}>
-                  {foods[k].nome}
-                </option>
-              ))}
-            </select>
-
-            <Label htmlFor="amount">
-              Quantidade
-              <input id="amount" name="amount" type="number" />
-            </Label>
-
-            <button type="submit">Adicionar Alimento</button>
-          </Form>
-
-          <table>
-            <thead>
-              <tr>
-                <td>Alimento</td>
-                <td>Quant.</td>
-              </tr>
-            </thead>
-
-            <tbody>
-              {Object.keys(addedFoods).map(k => (
-                <tr key={k}>
-                  <td style={{ width: '100%' }}>{addedFoods[k].name}</td>
-                  <td style={{ textAlign: 'right' }}>{addedFoods[k].amount}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </Main>
-      </React.Fragment>
-    );
-  }
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Foods foods={aw.getAlimentos()} />
+    </>
+  );
 }
 
 const GlobalStyle = createGlobalStyle`
@@ -101,42 +22,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen-Sans, Ubuntu,
     Cantarell, "Helvetica Neue", sans-serif;
   }
-`;
-
-const Header = styled.header`
-  margin-bottom: 1rem;
-  padding-right: 0.5rem;
-  padding-left: 0.5rem;
-`;
-
-const Title = styled.h1`
-  text-align: center;
-`;
-
-const Description = styled.p`
-  text-indent: 1ch;
-  line-height: 1.2;
-`;
-
-const Main = styled.main`
-  padding-right: 0.5rem;
-  padding-left: 0.5rem;
-`;
-
-const Form = styled.form`
-  position: sticky;
-  top: 0;
-  display: flex;
-  flex-direction: column;
-  background-color: #fff;
-  margin-bottom: 1rem;
-`;
-
-const Label = styled.label`
-  display: flex;
-  flex-direction: column;
-  margin-top: 0.5rem;
-  margin-bottom: 0.5rem;
 `;
 
 export default App;
---
+++ src/components/Foods.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.2rem;
+  margin-right: 0.5rem;
+  margin-left: 0.5rem;
+`;
+
+const List = styled.ul`
+  margin: 0;
+  padding-right: 0.5rem;
+  padding-left: 0.5rem;
+  list-style: none;
+`;
+
+const Item = styled.li`
+  &:not(:last-child) a {
+    border-bottom: 1px solid #eee;
+  }
+`;
+
+const Link = styled.a`
+  display: block;
+  font-size: 1rem;
+  line-height: 1;
+  color: #000;
+  text-decoration: none;
+  padding-top: 0.5rem;
+  padding-bottom: 0.5rem;
+`;
+
+function Foods({ foods }) {
+  return (
+    <>
+      <Title>Alimentos</Title>
+      <List>
+        {foods.map(f => (
+          <Item>
+            <Link key={f.id} href="#!">
+              {f.nome}
+            </Link>
+          </Item>
+        ))}
+      </List>
+    </>
+  );
+}
+
+Foods.propTypes = {
+  foods: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      nome: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Foods;
